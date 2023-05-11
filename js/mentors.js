bb913ca0f4d9f09b21a6d4b3e84ea854507ab08a
Aleyna Avcı <benaleynavci@gmail.com>
2023-05-11 23:28:00 +0300

    edited mentor information

--- js/mentors.js
+++ js/mentors.js
@@ -1,62 +1,153 @@
-const mentors = [
-  {
-    name: "Arda",
-    surname: "Yüksel",
-    socialMedia: [
-      {
-        linkedin: "https://www.linkedin.com/in/arda-y%C3%BCksel-a81a02221/",
-        displayName: "Linkedln",
-      },
-      {
-        github: "https://github.com/yukselarda",
-        displayName: "GitHub",
-      },
-      {
-        instagram: "https://www.instagram.com/yukselll_arda/",
-        displayName: "Instagram",
-      },
-    ],
-    school: "TEV İnanç Özel Türkeş Özel Listesi",
-    exp: ["html", "css", "js"],
-  },
-  {
-    name: "Aleyna",
-    surname: "Avcı",
-    socialMedia: [
-      {
-        linkedin: "https://www.linkedin.com/in/arda-y%C3%BCksel-a81a02221/",
-        displayName: "Linkedln",
-      },
-      {
-        github: "https://github.com/yukselarda",
-        displayName: "GitHub",
-      },
-      {
-        instagram: "https://www.instagram.com/yukselll_arda/",
-        displayName: "Instagram",
-      },
-    ],
-    school: "TEV İnanç Özel Türkeş Özel Listesi",
-    exp: ["html", "css", "js"],
-  },
-  {
-    name: "Selma",
-    surname: "Yüksel",
-    socialMedia: [
-      {
-        linkedin: "https://www.linkedin.com/in/arda-y%C3%BCksel-a81a02221/",
-        displayName: "Linkedln",
-      },
-      {
-        github: "https://github.com/yukselarda",
-        displayName: "GitHub",
-      },
-      {
-        instagram: "https://www.instagram.com/yukselll_arda/",
-        displayName: "Instagram",
-      },
-    ],
-    school: "TEV İnanç Özel Türkeş Özel Listesi",
-    exp: ["html", "css", "js"],
-  },
+const mentors = [{
+        name: "Aleyna",
+        surname: "Avcı",
+        socialMedia: [{
+                linkedin: "https://www.linkedin.com/in/aleynaavci/",
+                displayName: "Linkedln",
+            },
+            {
+                github: "https://www.linkedin.com/in/aleynaavci/",
+                displayName: "GitHub",
+            },
+            {
+                instagram: "",
+                displayName: "Instagram",
+            },
+        ],
+        school: "Sakarya Uygulamalı Bilimler Üniversitesi",
+        exp: ["html", "css", "js"],
+    },
+    {
+        name: "Arda",
+        surname: "Yüksel",
+        socialMedia: [{
+                linkedin: "https://www.linkedin.com/in/arda-y%C3%BCksel-a81a02221/",
+                displayName: "Linkedln",
+            },
+            {
+                github: "https://github.com/yukselarda",
+                displayName: "GitHub",
+            },
+            {
+                instagram: "https://www.instagram.com/yukselll_arda/",
+                displayName: "Instagram",
+            },
+        ],
+        school: " ",
+        exp: ["html", "css", "js"],
+    },
+    {
+        name: "Begüm Hilal",
+        surname: "Telli",
+        socialMedia: [{
+                linkedin: "https://wwwhttps://www.linkedin.com/in/beg%C3%BCm-hilal-telli-135243264/.linkedin.com/in/arda-y%C3%BCksel-a81a02221/",
+                displayName: "Linkedln",
+            },
+            {
+                github: "https://github.com/s-elg",
+                displayName: "GitHub",
+            },
+            {
+                instagram: "https://instagram.com/elginsum?igshid=ZGUzMzM3NWJiOQ==",
+                displayName: "Instagram",
+            },
+        ],
+        school: " ",
+        exp: ["html", "css", "js"],
+    },
+    {
+        name: "Çağrı",
+        surname: "Okan",
+        socialMedia: [{
+                linkedin: " ",
+                displayName: "Linkedln",
+            },
+            {
+                github: " ",
+                displayName: "GitHub",
+            },
+            {
+                instagram: " ",
+                displayName: "Instagram",
+            },
+        ],
+        school: " ",
+        exp: ["html", "css", "js"],
+    },
+    {
+        name: "Eren",
+        surname: "",
+        socialMedia: [{
+                linkedin: "",
+                displayName: "Linkedln",
+            },
+            {
+                github: "",
+                displayName: "GitHub",
+            },
+            {
+                instagram: "",
+                displayName: "Instagram",
+            },
+        ],
+        school: " ",
+        exp: ["html", "css", "js"],
+    },
+    {
+        name: "Furkan",
+        surname: "Karagöz",
+        socialMedia: [{
+                linkedin: " ",
+                displayName: "Linkedln",
+            },
+            {
+                github: " ",
+                displayName: "GitHub",
+            },
+            {
+                instagram: " ",
+                displayName: "Instagram",
+            },
+        ],
+        school: " ",
+        exp: ["html", "css", "js"],
+    },
+    {
+        name: "Selma",
+        surname: "Yüksel",
+        socialMedia: [{
+                linkedin: "https://www.linkedin.com/in/selmakoksal/",
+                displayName: "Linkedln",
+            },
+            {
+                github: "https://github.com/selmakoksal",
+                displayName: "GitHub",
+            },
+            {
+                instagram: " ",
+                displayName: "Instagram",
+            },
+        ],
+        school: " ",
+        exp: ["html", "css", "js"],
+    },
+    {
+        name: "Sümeyye",
+        surname: "Elgin",
+        socialMedia: [{
+                linkedin: " ",
+                displayName: "Linkedln",
+            },
+            {
+                github: " ",
+                displayName: "GitHub",
+            },
+            {
+                instagram: " ",
+                displayName: "Instagram",
+            },
+        ],
+        school: " ",
+        exp: ["html", "css", "js"],
+    },
 ];
