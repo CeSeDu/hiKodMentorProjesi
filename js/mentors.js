e756eadc2400b352d1de16fea7e25e5261cc0a0d
Aleyna Avcı <benaleynavci@gmail.com>
2023-05-11 22:55:11 +0300

    dynamizing the mentor page

---
+++ js/mentors.js
@@ -0,0 +1,62 @@
+const mentors = [
+  {
+    name: "Arda",
+    surname: "Yüksel",
+    socialMedia: [
+      {
+        linkedin: "https://www.linkedin.com/in/arda-y%C3%BCksel-a81a02221/",
+        displayName: "Linkedln",
+      },
+      {
+        github: "https://github.com/yukselarda",
+        displayName: "GitHub",
+      },
+      {
+        instagram: "https://www.instagram.com/yukselll_arda/",
+        displayName: "Instagram",
+      },
+    ],
+    school: "TEV İnanç Özel Türkeş Özel Listesi",
+    exp: ["html", "css", "js"],
+  },
+  {
+    name: "Aleyna",
+    surname: "Avcı",
+    socialMedia: [
+      {
+        linkedin: "https://www.linkedin.com/in/arda-y%C3%BCksel-a81a02221/",
+        displayName: "Linkedln",
+      },
+      {
+        github: "https://github.com/yukselarda",
+        displayName: "GitHub",
+      },
+      {
+        instagram: "https://www.instagram.com/yukselll_arda/",
+        displayName: "Instagram",
+      },
+    ],
+    school: "TEV İnanç Özel Türkeş Özel Listesi",
+    exp: ["html", "css", "js"],
+  },
+  {
+    name: "Selma",
+    surname: "Yüksel",
+    socialMedia: [
+      {
+        linkedin: "https://www.linkedin.com/in/arda-y%C3%BCksel-a81a02221/",
+        displayName: "Linkedln",
+      },
+      {
+        github: "https://github.com/yukselarda",
+        displayName: "GitHub",
+      },
+      {
+        instagram: "https://www.instagram.com/yukselll_arda/",
+        displayName: "Instagram",
+      },
+    ],
+    school: "TEV İnanç Özel Türkeş Özel Listesi",
+    exp: ["html", "css", "js"],
+  },
+];
