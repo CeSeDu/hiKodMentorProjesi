0f750bda5af56faef37f712a7e571636cf801d53
Sena Durmuş <senad8127@gmail.com>
2023-06-06 18:06:50 +0300

    fixed clear
    Signed-off-by: Sena Durmuş <senad8127@gmail.com>

--- hiKodMentorProjesi/js/scrollButton.js
+++
@@ -1,9 +0,0 @@
-let scrtollToTop = document.querySelector("#scrtollToTop");
-
-// başlangıca dönmek için olan scroll butonunu seçiyoruz.
-
-scrtollToTop.addEventListener("click", () => {
-  window.scrollTo(0, 0);
-});
-
-// scroll butonuna basılınca scrollTo işlevine 0,0 değerlerini vererek sayfanın başına dönmeyi sağlıyoruz.
