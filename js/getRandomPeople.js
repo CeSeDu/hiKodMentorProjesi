0cf630ecceb1c7765039d2639099410f33eae90e
Aleyna Avcı <benaleynavci@gmail.com>
2023-06-14 19:48:24 +0300

    added RandomPeople no photo option to mentor cards

--- js/getRandomPeople.js
+++ js/getRandomPeople.js
@@ -1,53 +1,57 @@
 const randomUsers = [];
 
 const getRandomValue = () => {
-  return Math.floor(Math.random() * mentors.length);
+    return Math.floor(Math.random() * mentors.length);
 };
 const getRandomUser = () => {
-  let user = mentors[getRandomValue()];
-  if (randomUsers.length > 0) {
-    while (true) {
-      if (
-        randomUsers[0].name === user.name &&
-        randomUsers[0].surname === user.surname
-      ) {
-        user = mentors[getRandomValue()];
-      } else {
+    let user = mentors[getRandomValue()];
+    if (randomUsers.length > 0) {
+        while (true) {
+            if (
+                randomUsers[0].name === user.name &&
+                randomUsers[0].surname === user.surname
+            ) {
+                user = mentors[getRandomValue()];
+            } else {
+                randomUsers.push(user);
+                return false;
+            }
+        }
+    } else {
         randomUsers.push(user);
-        return false;
-      }
+        return getRandomUser();
     }
-  } else {
-    randomUsers.push(user);
-    return getRandomUser();
-  }
 };
 getRandomUser();
 
 const img = document.querySelectorAll(".mentorImg > img");
 const mentorName = document.querySelectorAll(".mentorInfo > h3");
 const mentorSocialLinkedin = document.querySelectorAll(
-  ".mentorContact > div:nth-child(2) > span"
+    ".mentorContact > div:nth-child(2) > span"
 );
 const mentorSocialInstagram = document.querySelectorAll(
-  ".mentorContact > div:nth-child(1) > span"
+    ".mentorContact > div:nth-child(1) > span"
 );
 
 randomUsers.map((user, indx) => {
-  img[indx].src = `/img/img-${user.name
+    if (`${user.img}`.length > 0) {
+        img[indx].src = `/img/img-${user.name
     .toLocaleLowerCase()
     .turkishToEnglish()}.jpg`;
-  mentorName[indx].innerHTML = `${user.name} ${user.surname}`;
-  mentorSocialLinkedin[indx].innerHTML = `${user.name} ${user.surname}`;
-  const userInstagram = user.socialMedia.find((socialMedia) => {
-    const socialMediaName = Object.entries(socialMedia)[0];
-    if (socialMediaName[0] === "instagram") {
-      return socialMedia;
+    } else {
+        img[indx].src = "/img/img-nophoto.jpg"
     }
-  });
-  if (userInstagram.instagram.length > 0) {
-    mentorSocialInstagram[indx].innerHTML = userInstagram.instagram
-      .replace("https://www.instagram.com/", "")
-      .slice(0, -1);
-  }
+    mentorName[indx].innerHTML = `${user.name} ${user.surname}`;
+    mentorSocialLinkedin[indx].innerHTML = `${user.name} ${user.surname}`;
+    const userInstagram = user.socialMedia.find((socialMedia) => {
+        const socialMediaName = Object.entries(socialMedia)[0];
+        if (socialMediaName[0] === "instagram") {
+            return socialMedia;
+        }
+    });
+    if (userInstagram.instagram.length > 0) {
+        mentorSocialInstagram[indx].innerHTML = userInstagram.instagram
+            .replace("https://www.instagram.com/", "")
+            .slice(0, -1);
+    }
 });
