2c5a5f41ebe7518eeb154c8ef625cd137e7d9a1a
Aleyna Avcı <benaleynavci@gmail.com>
2023-06-14 13:14:13 +0300

    edited mentor photos

--- js/mentors.js
+++ js/mentors.js
@@ -18,7 +18,8 @@ const mentors = [{
         exp: ["html", "css", "git", "bootstrap", "js", "react"],
         age: 21,
         desc: "Merhaba, ben Aleyna Avcı. Sakarya Uygulamalı Bilimler Üniversitesi'nde Bilgisayar mühendisliği öğrencisiyim. Ankara'da yaşıyorum. Bir yılı aşkın süredir Front-End konusunda Javascript ve React ağırlıklı kendimi geliştirmeye çalışıyorum. Kariyerimin de bu yönde olmasını hedeflemekteyim. Mart 2023'ten bu yana Hi-Kod atölyesinde Front-End mentorluğu yapıyorum. Ayrıca kaktüsüm <a href='https://i.ibb.co/25pTVs2/IMG-20230610-WA0004.jpg' target='_blank'>Şukufe'yle</a>  birlikte Turkish psychedelic rock eşliğinde kod yazmaya bayılırım.",
-        video: "https://www.youtube.com/embed/R64q1NcgH0o"
+        video: "https://www.youtube.com/embed/R64q1NcgH0o",
+        img: "completed"
     },
     {
         name: "Ayşe Asude",
@@ -40,7 +41,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 18,
         desc: "Merhabalar ben Asude. İki buçuk yıldır siber güvenlik, oyun geliştirme, yapay zeka, front-end, back-end, veri bilimi, derin öğrenme ve makine öğrenmesi alanlarıyla ilgileniyorum. Kodluyoruz Jr.'de atölye gerçekleştirdim. Kodluyoruz Jr. Fellow'uyum.",
-        video: ""
+        video: "",
+        img: ""
     },
     {
         name: "Ayşenur",
@@ -62,7 +64,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 88,
         desc: "Ben Ayşenur Laklak. Süleyman Demirel üniversitesinde bilgisayar mühendisliği öğrencisiyim. Kitap okumayı, alışveriş yapmayı ve yeni şeyler keşfetmeyi severim.",
-        video: "https://www.youtube.com/embed/rDLXNq4nVQ8"
+        video: "https://www.youtube.com/embed/rDLXNq4nVQ8",
+        img: "completed"
     },
     {
         name: "Arda",
@@ -84,7 +87,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 16,
         desc: "Merhabalar ben Arda. Mehmet Rıfat Evyap Mesleki ve Teknik Anadolu Lisesi'nde bilişim bölümünü okuyorum. Kendimi iki yıldır Front-End alanında geliştirmeye çalışıyorum. 2023 yılının Mart ayında Kodluyoruz'da Hi-Kod Mentor görevi ile Gönüllülük sürecime başladım. Şiir yazmakla ilgileniyorum. Digimondi şirketinde eğitim görüyorum.",
-        video: ""
+        video: "",
+        img: "completed"
     },
     {
         name: "Azat",
@@ -106,7 +110,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 21,
         desc: "Merhabalar Ben Azat, Front-end ve UI / UX Tasarım alanına meraklı bir Web Geliştiriciyim. Ağırlıklı olarak JavaScript (ReactJS, NextJS) kullanıyorum 2020 yılından beridir Front-end alanına 2022 Yılından beridir de UI / UX Tasarım alanına ilgiliyim 2023 Yılının Mart ayında Kodluyoruza (Teknik Asistan) görevi ile Gönüllülük sürecime başladım Kodluyoruz içinde Açık Kaynak Kodluyoruz projelerinde çeşitli görevler aldım. Hİ Kod Atölyesinde yazılıma yeni başlayan insanlara Front-end alanında mentörlük yaptım ve Çeşitli projelerde UI / UX Tasarım alanında bildiklerimi Topluluk önünde anlatmaya çalıştım.",
-        video: "https://www.youtube.com/embed/rm2RRSy2KLU"
+        video: "https://www.youtube.com/embed/rm2RRSy2KLU",
+        img: "completed"
     },
     {
         name: "Begüm Hilal",
@@ -128,7 +133,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 22,
         desc: "Merhaba ben Begüm, bilgisayar mühendisliği öğrencisiyim. 2 senedir bu sektörün içindeyim.  Front end web geliştirme ve backend tarafında Java ile ilgilenmekteyim. 2023 Şubat ayından beri Hi Kod Front End atölyesinde mentorlük yapıyorum. Hedefim full stack developer olmak. Amacım ise kendi versiyonumun en iyisi olup sektörde kadın gücünü göstermek! Yazılım sektöründe nice kadınları görmek ümidi ile!",
-        video: ""
+        video: "",
+        img: "completed"
     },
     {
         name: "Büşra",
@@ -150,7 +156,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 88,
         desc: "Merhaba! Ben Büşra Çelikçioğlu. 24 yaşındayım ve Kocaeli'de yaşıyorum. Kocaeli Üniversitesi'nde Bilişim Sistemleri Mühendisliği okuyorum ve aynı zamanda Bilgisayar Programcılığı mezunuyum. Yazılım geliştirme konusunda kendimi sürekli ilerletmeye çalışıyorum, özellikle Front-end web geliştirme ve Siber Güvenlik alanlarına ilgim büyük. Mezun olduğumda bu alanlarda çalışmayı hedefliyorum. Teknoloji dünyasındaki hızlı değişimleri yakından takip ederek ve kendimi geliştirmek için çaba sarf ediyorum. Ayrıca, müzik dinlemek, gezmek ve sosyal medya gibi ilgi alanlarıma da vakit ayırıyorum.",
-        video: ""
+        video: "",
+        img: "completed"
     },
     {
         name: "Çağrı",
@@ -172,7 +179,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 88,
         desc: "Merhaba, ben Çağrı! Bu kısımda uzun uzun kendim hakkında açıklama yapıyorum bla bla, mentorlere özel bir sürü kişisel şey konulabilir buraya (örnek: film listeleri, hi-kod dersleri, projeler vs.)",
-        video: ""
+        video: "",
+        img: "completed"
     },
     {
         name: "Eren Efe",
@@ -194,7 +202,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 17,
         desc: "Ben Eren 1 yıldır yazılımla , 6 aydır front-end web geliştirme ile ilgileniyorum. Hedefim full-stack web developer olmak.",
-        video: ""
+        video: "",
+        img: ""
     },
     {
         name: "Esmanur",
@@ -216,7 +225,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 88,
         desc: "Merhaba, ben Esmanur Karataş. 2022 yılında Fırat Üniversitesi Bilgisayar Programcılığı bölümünden mezun oldum. Şimdi de Anadolu Üniversitesi Yönetim Bilişim sistemleri okuyorum. Mezun olduktan sonra frontend alanına yönelip kendimi geliştirmeye başladım. Geliştirme sürecinde Kodluyoruz'da gönüllü teknik asistan olarak seçildim. Ayrıca frontend konusunda ilerlemem için yardımcı olabilecek insanlarla tanışmak ve bilgilerimi tazelemek amacıyla Hi-Kod 8. atölyeye başvurdum ve seçildim. Sonrasında 8. atölyede mentorluk yapmaya başladım.",
-        video: ""
+        video: "",
+        img: ""
     },
     {
         name: "Furkan",
@@ -238,7 +248,8 @@ const mentors = [{
         exp: ["html", "css", "csharp", "dotnet"],
         age: 18,
         desc: "Merhaba ben Furkan, 18 yaşındayım, üniversite sınavına hazırlanıyorum. Bir yandan da kendimi yazılım alanında geliştiriyorum. Şuan ilgimi çeken alan Backend Web geliştirme alanı. Bugünlerde Asp.net üzerine çalışıyorum. Onun dışında Müzik dinlemek, oyun oynamak ve bisiklete binmekten hoşlanırım.",
-        video: "https://www.youtube.com/embed/86NQ23OjUC0"
+        video: "https://www.youtube.com/embed/86NQ23OjUC0",
+        img: "completed"
     },
     {
         name: "Melisa",
@@ -260,7 +271,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 88,
         desc: "Merhaba ben Melisa. Lise dönemimde web tasarım eğitimine başlayarak temel bilgilere sahip oldum ve bu alanda deneyim kazandım. Şu an Işık Üniversitesi Bilgisayar Programcılığı son sınıf öğrencisiyim. Hedefim front-end alanında yeteneklerimi ilerletmek . Bu doğrultuda kendimi sürekli geliştirmek  için projeler üzerinde çalışarak deneyim ediniyorum.",
-        video: "https://www.youtube.com/embed/F63dCwoI0cQ"
+        video: "https://www.youtube.com/embed/F63dCwoI0cQ",
+        img: "completed"
     },
 
     {
@@ -283,7 +295,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 88,
         desc: "Merhaba ,ismim Nida. Firat Üniversitesi Yazılım Mühendisliği öğrencisi olarak eğitimime devam ediyorum. Kariyer hedeflerim doğrultusunda farklı alanlarda kendimi geliştiriyorum. Her fırsatta kendime yeni deneyimler katmak için itina ile çalışıyorum.",
-        video: "https://youtube.com/embed/ZpM62phJPtg?feature=share"
+        video: "https://youtube.com/embed/ZpM62phJPtg?feature=share",
+        img: "completed"
     },
     {
         name: "Nuran",
@@ -305,7 +318,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 22,
         desc: "Merhaba ben Nuran. Pamukkale Üniversitesinde 3. Sınıf öğrencisiyim. 1 Yıldır yazılımda kendimi geliştirmeye çalışıyorum. Şu an Hi-Kod Atölyesinde Front-End Mentorluk yapıyorum.",
-        video: ""
+        video: "",
+        img: "completed"
     },
     {
         name: "Seda",
@@ -327,7 +341,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 88,
         desc: "Merhabalar, ben Seda Toraman. Endüstri Mühendisliği son sınıf öğrencisiyim.    Anlık hayatımın içinde dijital bir işe yer verme fikri her zaman ilgimi çekiyordu, yazılım yolculuğumda bu düşüncemle birlikte başladı. Gitar çalmayı o kadar çok seviyorum ki, hem elimde gitar hem masamda bilgisayarımla birlikte sosyal hayatımdan geri kalmadım diyebilirim. Kayak yapmayı, badminton oynamayı, gezmeyi de sevdiğim kadar Hi-Kod Atölyesiyle tanıştığımdan beri ufak da olsa projeler geliştirmek oldukça eğlenceli. ",
-        video: "https://www.youtube.com/embed/gdg9oYUCgyg"
+        video: "https://www.youtube.com/embed/gdg9oYUCgyg",
+        img: "completed"
     },
     {
         name: "Selma",
@@ -349,7 +364,8 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 21,
         desc: "Merhabalar ben Selma. Kendimi iki yıldır Front-End alanında geliştirmeye çalışıyorum. 2023 yılının Mart ayında Kodluyoruz'da Sosyal Asistan görevi ile Gönüllülük sürecime başladım. 2022 yılı Nisan ayından beri Hi Kod Front End atölyesinde mentorlük yaparak yazılıma yeni başlayan insanlara yardımcı olmaya çalışıyorum.",
-        video: ""
+        video: "",
+        img: "completed"
     },
     {
         name: "Sümeyye",
@@ -371,6 +387,7 @@ const mentors = [{
         exp: ["html", "css", "js"],
         age: 21,
         desc: "Merhaba, ben Sümeyye! Bilgisayar mühendisliği 1. sınıf öğrencisiyim. Çanakkale'de yaşıyorum. Kendimi front-end alanında geliştirmekteyim. Hi-Kod Front-end 7. atölyesinden mezun oldum. 2023 yılının mart ayından beri gönüllü mentörlük yapmaktayım.",
-        video: "https://www.youtube.com/embed/zsDaLQLA1ls"
+        video: "https://www.youtube.com/embed/zsDaLQLA1ls",
+        img: "completed"
     },
 ];
